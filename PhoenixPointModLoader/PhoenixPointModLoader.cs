325aab0e6d2cb6483834232fdbbc156f1c12553b
Hussein Farran <hmfarran@gmail.com>
2020-01-29 22:31:54 -0500

    Code simplified in entrypoint class. Logger altered to contain its own init logic. PPML main class now somewhat more reasonably arranged.

--- PhoenixPointModLoader/PhoenixPointModLoader.cs
+++ PhoenixPointModLoader/PhoenixPointModLoader.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Reflection;
 using Harmony;
 using PhoenixPointModLoader.Mods;
-using static PhoenixPointModLoader.Logger;
 
 namespace PhoenixPointModLoader
 {
@@ -20,43 +19,26 @@ namespace PhoenixPointModLoader
 
 		public static string ModDirectory { get; private set; }
 
-	
-
 		public static void Initialize()
 		{
 			string manifestDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
-				?? throw new InvalidOperationException("Manifest path is invalid.");
+				?? throw new InvalidOperationException("Could not determine operating directory. Is your folder structure correct? " +
+				"Try verifying game files in the Epic Games Launcher, if you're using it.");
 
-
-			// this should be (wherever Phoenix Point is Installed)\PhoenixPoint\PhoenixPointWin64_Data\Managed
-			ModDirectory = Path.GetFullPath(
-				Path.Combine(manifestDirectory, Path.Combine(@"..\..\Mods")));
-
-			LogPath = Path.Combine(ModDirectory, "PPModLoader.log");
-
-			Version PPMLVersion = Assembly.GetExecutingAssembly().GetName().Version;
+			ModDirectory = Path.GetFullPath(Path.Combine(manifestDirectory, Path.Combine(@"..\..\Mods")));
 
 			if (!Directory.Exists(ModDirectory))
 				Directory.CreateDirectory(ModDirectory);
 
-			// create log file, overwriting if it's already there
-			using (var logWriter = File.CreateText(LogPath))
-			{
-				logWriter.WriteLine($"PPModLoader -- PPML v{PPMLVersion} -- {DateTime.Now}");
-			}
+			Logger.InitializeLogging(Path.Combine(ModDirectory, "PPModLoader.log"));
 
-			// ReSharper disable once UnusedVariable
-			var harmony = HarmonyInstance.Create("io.github.realitymachina.PPModLoader");
+			IList<IPhoenixPointMod> allMods = RetrieveAllMods();
+			InitializeMods(allMods);
+		}
 
-			// get all dll paths
+		private static IList<IPhoenixPointMod> RetrieveAllMods()
+		{
 			List<string> dllPaths = Directory.GetFiles(ModDirectory, "*.dll", SearchOption.AllDirectories).ToList();
-
-			if (!dllPaths.Any())
-			{
-				Log(@"No .DLLs loaded. DLLs must be placed in the root of the folder \PhoenixPoint\Mods\.");
-				return;
-			}
-
 			List<IPhoenixPointMod> allMods = new List<IPhoenixPointMod>();
 			IncludeDefaultMods(allMods);
 			foreach (var dllPath in dllPaths)
@@ -64,41 +46,37 @@ namespace PhoenixPointModLoader
 				if (!IGNORE_FILE_NAMES.Contains(Path.GetFileName(dllPath)))
 					allMods.AddRange(LoadDll(dllPath));
 			}
-
-			InitializeMods(allMods);	
+			return allMods;
 		}
 
-		private static void IncludeDefaultMods(List<IPhoenixPointMod> allMods)
+		private static void IncludeDefaultMods(IList<IPhoenixPointMod> allMods)
 		{
-			allMods.AddRange(new IPhoenixPointMod[]
-			{
-				new EnableConsoleMod(),
-				new LoadConsoleCommandsFromAllAssembliesMod()
-			});
+			allMods.Add(new EnableConsoleMod());
+			allMods.Add(new LoadConsoleCommandsFromAllAssembliesMod());
 		}
 
-		private static void InitializeMods(List<IPhoenixPointMod> allMods)
+		private static void InitializeMods(IList<IPhoenixPointMod> allMods)
 		{
 			var prioritizedModList = allMods.ToLookup(x => x.Priority);
 			ModLoadPriority[] loadOrder = new[] { ModLoadPriority.High, ModLoadPriority.Normal, ModLoadPriority.Low };
 			foreach (ModLoadPriority priority in loadOrder)
 			{
-				Log("Attempting to initialize `{0}` priority mods.", priority.ToString());
+				Logger.Log("Attempting to initialize `{0}` priority mods.", priority.ToString());
 				foreach (var mod in prioritizedModList[priority])
 				{
 					try
 					{
 						mod.Initialize();
-						Log("Mod class `{0}` from DLL `{1}` was successfully initialized.",
+						Logger.Log("Mod class `{0}` from DLL `{1}` was successfully initialized.",
 							mod.GetType().Name,
 							Path.GetFileName(mod.GetType().Assembly.Location));
 					}
 					catch (Exception e)
 					{
-						Log("Mod class `{0}` from DLL `{1}` failed to initialize.",
+						Logger.Log("Mod class `{0}` from DLL `{1}` failed to initialize.",
 							mod.GetType().Name,
 							Path.GetFileName(mod.GetType().Assembly.Location));
-						Log(e.ToString());
+						Logger.Log(e.ToString());
 					}
 				}
 			}
@@ -114,7 +92,7 @@ namespace PhoenixPointModLoader
 
 			if (!modClasses.Any())
 			{
-				Log("No mod classes found in DLL: {0}", Path.GetFileName(path));
+				Logger.Log("No mod classes found in DLL: {0}", Path.GetFileName(path));
 				return new List<IPhoenixPointMod>();
 			}
 
@@ -128,14 +106,15 @@ namespace PhoenixPointModLoader
 				}
 				catch (Exception e)
 				{
-					Log("Error has occurred when instantiating mod class`{0}` in DLL `{1}`.", modClass.Name, Path.GetFileName(path));
-					Log(e.ToString());
+					Logger.Log("Error has occurred when instantiating mod class`{0}` in DLL `{1}`.", modClass.Name, Path.GetFileName(path));
+					Logger.Log(e.ToString());
 					continue;
 				}
 
 				if (modInstance == null)
 				{
-					Log("Instantiated mod class `{0}` from DLL `{1}` was null for unknown reason.", modClass.Name, Path.GetFileName(path));
+					Logger.Log("Instantiated mod class `{0}` from DLL `{1}` was null for unknown reason. " +
+						"Please ensure you have a default constructor defined for your type.", modClass.Name, Path.GetFileName(path));
 				}
 
 				modInstances.Add(modInstance);
